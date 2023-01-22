14c1ddf2363a0b8dc3445d0c67762699e14e23cc
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2023-01-22 13:02:58 +0100

    adding the state and Axios

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,32 +1,64 @@
+import axios from 'axios';
+import { Component } from 'react';
 import './App.css';
 import PlayerForm from './Player/PlayerForm';
 import PlayerList from './Player/PlayerList';
 import PlayerSingle from './Player/PlayerSingle';
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className='row'>
-        <div className='col s12'>
-          Menu
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col s3'>
-          <PlayerList />
-        </div>
-        <div className='col s9'>
-          <PlayerSingle />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col s12'>
-          <PlayerForm />
-        </div>
-      </div>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      players: [],
+      currentPlayer: null
+    }
+    this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this)
+  }
 
-    </div>
-  );
+  updateCurrentPlayer = (item) => {
+    this.setState({
+      currentPlayer: item
+    })
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:3500/players'
+
+    axios.get(url).then((res) => {
+      this.setState({
+        players: res.data
+      })
+    }).catch((err) => console.error(err))
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className='row'>
+          <div className='col s12'>
+            Menu
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col s3'>
+            <PlayerList />
+          </div>
+          <div className='col s9'>
+            <PlayerSingle
+              players={this.state.players}
+              updateCurrentPlayer={this.updateCurrentPlayer}
+            />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col s12'>
+            <PlayerForm />
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
